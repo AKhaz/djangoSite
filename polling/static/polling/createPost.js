6123e3f6c5442d406351620db0ab5a1fb88bbf5b
AKhaz <aiden.khazeni.22@bishops.com>
2021-05-26 11:41:28 -0700

    CreatePost Update and Basic JS

---
+++ polling/static/polling/createPost.js
@@ -0,0 +1,4 @@
+function createNewChoice(){
+  var choiceDiv = document.getElementById("NewChoiceDiv")
+  choiceDiv.innerHTML += "<input class='InputFormBox' type='text' name = 'Option1' placeholder='Option 1'></input>"
+}
